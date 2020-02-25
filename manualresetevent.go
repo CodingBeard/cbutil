dcc8e5f030864bece24f49513156d127ac00254f
Tim Marshall <tim@codingbeard.com>
2020-02-25 12:01:00 +0000

    Add waiters count to manual reset event to allow multiple waits and make sure they all get notified on .Set() Add multi manual reset event object

--- manualresetevent.go
+++ manualresetevent.go
@@ -3,16 +3,19 @@ package cbutil
 import "sync"
 
 type ManualResetEvent struct {
-	lock   *sync.Mutex
-	locked bool
-	wait   chan int
+	lock        *sync.Mutex
+	locked      bool
+	wait        chan int
+	waitersLock *sync.Mutex
+	waiters     int
 }
 
 func NewManualResetEvent(locked bool) *ManualResetEvent {
 	event := ManualResetEvent{
-		lock:   &sync.Mutex{},
-		locked: locked,
-		wait:   make(chan int),
+		lock:        &sync.Mutex{},
+		waitersLock: &sync.Mutex{},
+		locked:      locked,
+		wait:        make(chan int),
 	}
 
 	return &event
@@ -20,6 +23,9 @@ func NewManualResetEvent(locked bool) *ManualResetEvent {
 
 func (e *ManualResetEvent) Wait() {
 	if e.locked {
+		e.waitersLock.Lock()
+		e.waiters++
+		e.waitersLock.Unlock()
 		<-e.wait
 	}
 }
@@ -29,11 +35,43 @@ func (e *ManualResetEvent) Set() {
 	e.locked = false
 	e.lock.Unlock()
 
-	e.wait <- 1
+	for i := 0; i < e.waiters; i++ {
+		e.wait <- 1
+	}
 }
 
 func (e *ManualResetEvent) Reset() {
 	e.lock.Lock()
 	e.locked = true
 	e.lock.Unlock()
+}
+
+type MultiManualResetEvent struct {
+	events    map[string]*ManualResetEvent
+	eventLock *sync.Mutex
+}
+
+func NewMultiManualResetEvent() *MultiManualResetEvent {
+	return &MultiManualResetEvent{
+		eventLock: &sync.Mutex{},
+		events:    make(map[string]*ManualResetEvent),
+	}
+}
+
+func (m *MultiManualResetEvent) Get(key string, locked bool) (isNew bool, event *ManualResetEvent) {
+	m.eventLock.Lock()
+	defer m.eventLock.Unlock()
+	if event, ok := m.events[key]; ok {
+		return false, event
+	}
+
+	m.events[key] = NewManualResetEvent(locked)
+
+	return true, m.events[key]
+}
+
+func (m *MultiManualResetEvent) Remove(key string) {
+	m.eventLock.Lock()
+	delete(m.events, key)
+	m.eventLock.Unlock()
 }

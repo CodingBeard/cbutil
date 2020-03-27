1ebbe6021a60deba76cf285e785166be95f30d8e
Tim Marshall <tim@codingbeard.com>
2020-03-27 14:07:12 +0000

    Add configurable domain to dev browser refresh

--- devbrowserrefresh.go
+++ devbrowserrefresh.go
@@ -1,6 +1,7 @@
 package cbutil
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -8,24 +9,24 @@ import (
 	"time"
 )
 
-func DevRefreshBrowserOnLoadAndTemplateChange() {
+func DevRefreshBrowserOnLoadAndTemplateChange(domain string) {
 	if os.Getenv("CHROMEREFRESH") == "1" && runtime.GOOS == "darwin" {
 		go func() {
 			Sleep(time.Second)
 
-			cmd := exec.Command(`/usr/bin/osascript`, `-e`, `tell application "Google Chrome"
+			cmd := exec.Command(`/usr/bin/osascript`, `-e`, fmt.Sprintf(`tell application "Google Chrome"
 	set window_list to every window # get the windows
 
 	repeat with the_window in window_list # for every window
 		set tab_list to every tab in the_window # get the tabs
 
 		repeat with the_tab in tab_list # for every tab
-			if the URL of the_tab contains "localhost" then
+			if the URL of the_tab contains "%s" then
 				tell the_tab to reload
 			end if
 		end repeat
 	end repeat
-end tell`)
+end tell`, domain))
 			e := cmd.Run()
 			if e != nil {
 				log.Println(e.Error())
@@ -36,19 +37,19 @@ end tell`)
 				log.Println(e.Error())
 			}
 
-			watch := exec.Command(`/bin/bash`, `-c`, `/usr/local/bin/fswatch -e ".*" -i "\\.gohtml$" -o . | xargs -n1 -I {} osascript -e 'tell application "Google Chrome"
+			watch := exec.Command(`/bin/bash`, `-c`, fmt.Sprintf(`/usr/local/bin/fswatch -e ".*" -i "\\.gohtml$" -o . | xargs -n1 -I {} osascript -e 'tell application "Google Chrome"
 	set window_list to every window # get the windows
 
 	repeat with the_window in window_list # for every window
 		set tab_list to every tab in the_window # get the tabs
 
 		repeat with the_tab in tab_list # for every tab
-			if the URL of the_tab contains "localhost" then
+			if the URL of the_tab contains "%s" then
 				tell the_tab to reload
 			end if
 		end repeat
 	end repeat
-end tell'`)
+end tell'`, domain))
 			e = watch.Start()
 			if e != nil {
 				log.Println(e.Error())

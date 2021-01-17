2fa33c82c5fd9e97144d5998dd5ba1cd2bfc139c
Tim Marshall <tim@codingbeard.com>
2021-01-17 12:56:23 +0000

    Fix stats for windows and darwin

---
+++ stats_linux.go
@@ -0,0 +1,8 @@
+package cbutil
+
+// #include <unistd.h>
+import "C"
+
+func GetTotalMemory() int64 {
+	return int64(C.sysconf(C._SC_PHYS_PAGES)*C.sysconf(C._SC_PAGE_SIZE))
+}
---
+++ stats_windows.go
@@ -0,0 +1,8 @@
+package cbutil
+
+// #include <unistd.h>
+import "C"
+
+func GetTotalMemory() int64 {
+	return 0
+}
